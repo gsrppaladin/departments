e616fad31379438890fe3ebfb1367e4852116c2a
Sam Greenhill <samgreenhill@Sams-MBP.home>
2017-04-16 20:05:38 -0400

    creating all the classes for the view Controllers.

---
+++ departments/EmployeeTVCell.swift
@@ -0,0 +1,24 @@
+//
+//  EmployeeTVCell.swift
+//  departments
+//
+//  Created by Sam Greenhill on 4/16/17.
+//  Copyright © 2017 simplyAmazingMachines. All rights reserved.
+//
+
+import UIKit
+
+class EmployeeTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
