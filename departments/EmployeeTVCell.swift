b59f5872913bd89c3772f522c1888abedccc1cf0
Sam Greenhill <samgreenhill@Sams-MBP.home>
2017-04-30 14:41:52 -0400

    Editing the employee tableView like the other tableViews

--- departments/EmployeeTVCell.swift
+++ departments/EmployeeTVCell.swift
@@ -10,15 +10,22 @@ import UIKit
 
 class EmployeeTVCell: UITableViewCell {
 
+    
+    @IBOutlet var nameLbl: UILabel!
+    
+    @IBOutlet var designationLbl: UILabel!
+    
+    @IBOutlet var destinationLbl: UILabel!
+    
+    
+    
+    
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
 }
